d606517c58ca3298973b4d6712a9c976dc4616ee
Pandaliha <25638438+Pandaliha@users.noreply.github.com>
2018-03-15 17:43:24 +0100

    Remove last added article

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 'use strict';
 
-angular.module('tutorialApp', [])
-  .controller('ArticlesCtrl', function($scope){
-    $scope.articles = [
-      { id: 1, name: 'It was a dark', price: 100 },
-      { id: 2, name: 'and stormy night', price: 250 },
-      { id: 3, name: 'the rain fell in torrents', price: 160 },
-      { id: 4, name: 'except at occasional intervals', price: 330 },
-      { id: 5, name: 'when it was checked by a violent gust', price: 255 },
-      { id: 6, name: 'of wind', price: 116 },
-      { id: 7, name: 'which swept up the streets', price: 156 },
-      { id: 8, name: '(for it is in London that our scene lies)', price: 200 },
-      { id: 9, name: ' rattling along the housetops', price: 430 },
-      { id: 10, name: 'and fiercely agitating the scanty', price: 120 },
-      { id: 11, name: 'flame of the lamps', price: 140 },
-      { id: 12, name: 'that struggled against', price: 120 },
-      { id: 13, name: 'the darkness', price: 100 }
-    ];
+angular.module('tutorialApp', ['ngAnimate', 'ngRoute'])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', { templateUrl: 'articles.html' })
+      .otherwise({ redirectTo: '/'});
+  })
+  .directive('price', function(){
+    return {
+      restrict: 'E',
+      scope: {
+        value: '='
+      },
+      template: '<span ng-show="value == 0">kostenlos</span>' +
+        '<span ng-show="value > 0">{{value | currency}}</span>'
+    }
+  })
+  .factory('Cart', function() {
+    var items = [];
+    return {
+      getItems: function() {
+        return items;
+      },
+      addArticle: function(article) {
+        items.push(article);
+      },
+      removeArticle: function(article){
+        var rem = array.indexOf(article);
+        if (rem > -1) {
+          array.splice(index, 1);
+        }
+      },
+      reverseArticle: function() {
+        items.pop();
+      },
+      sum: function() {
+        return items.reduce(function(total, article) {
+          return total + article.price;
+        }, 0);
+      }
+    };
+  })
+  .controller('ArticlesCtrl', function($scope, $http, Cart){
+    $scope.cart = Cart;
+    $http.get('articles.json').then(function(articlesResponse) {
+      $scope.articles = articlesResponse.data;
+    });
+  })
+  .controller('CartCtrl', function($scope, Cart){
+    $scope.cart = Cart;
   });
